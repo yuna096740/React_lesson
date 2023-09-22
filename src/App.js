2e2140147c77022a58fe989c4f38bac1ae66fb40
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-22 14:33:47 +0900

    [Initial Commit ] React lesson

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Lesson test view!
         </p>
         <a
           className="App-link"
