bb43a4f7f86124a58fc1d46d5415008a7527ea3c
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-23 10:34:47 +0900

    [add] Tasks counts in heddingTitle

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Form from "./components/Form";
 import FillterButton from "./components/FilterButton";
 
 function App(props) {
-
-  const [tasks, setTasks] = useState(props.tasks);
-
+  
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false};
     // スプレッド演算子(...)
     setTasks([...tasks, newTask]);
   }
-
+  
+  const [tasks, setTasks] = useState(props.tasks);
   const subject = props.subject;
-
+  
   const taskList = tasks.map((task) => 
-    <Todo 
-      id ={task.id} 
-      name={task.name} 
-      completed={task.completed} 
-      key={task.id}
-    />
+  <Todo 
+  id ={ task.id } 
+  name={ task.name } 
+  completed={ task.completed }
+  key={ task.id }
+  />
   );
+  
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${ taskList.length } ${ tasksNoun } remaining`;
 
   return (
     <div className="todoapp stack-large">
       <h1>Todo{ subject }</h1>
 
-        <Form addTask={addTask} />
+        <Form addTask={ addTask } />
 
       <div className="filters btn-group stack-exception">
         <FillterButton name="All"/>
@@ -37,14 +39,14 @@ function App(props) {
         <FillterButton name="Completed"/>
       </div>
 
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{ headingText }</h2>
 
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         
-        {taskList}
+        { taskList }
       </ul>
     </div>
   );
