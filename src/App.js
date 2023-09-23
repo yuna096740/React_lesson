61383138a67766528fdf4e08a0ba8384a42d5bd0
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-23 13:11:13 +0900

    [add] toggleTaskCompleted function

--- src/App.js
+++ src/App.js
@@ -15,12 +15,23 @@ function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const subject = props.subject;
   
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
   const taskList = tasks.map((task) => 
   <Todo 
   id ={ task.id } 
   name={ task.name } 
   completed={ task.completed }
   key={ task.id }
+  toggleTaskCompleted={toggleTaskCompleted}
   />
   );
   
