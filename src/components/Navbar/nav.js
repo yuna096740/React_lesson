e750f1c53387ae8d783d8eff0b1533fab73b4ae4
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-27 09:21:37 +0900

    [add] Home, About

--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     <nav className="nav">
       <ul>
         <li><a href="./index.html">Todo</a></li>
-        <li><a href="./index.html">Home</a></li>
-        <li><a href="./index.html">About</a></li>
+        <li><a href="./Home.html">Home</a></li>
+        <li><a href="./About.html">About</a></li>
       </ul>
     </nav>
   )
